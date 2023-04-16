6e9d625eec7036d8b8c72a809c20674e8f062958
miguel <miguelpositivo02@gmail.com>
2023-04-15 21:58:55 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite alguma coisa:");
+string coisa = Console.ReadLine()!;
+Console.WriteLine(coisa.ToUpper());
